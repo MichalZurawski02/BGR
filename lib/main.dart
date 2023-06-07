e4889a2faded0e30806ce5fa270044117717901b
Maksymilian Neumann <59446967+Spiryd@users.noreply.github.com>
2023-06-07 08:19:35 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-FirebaseDAO firebaseDAO = FirebaseDAO();
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
